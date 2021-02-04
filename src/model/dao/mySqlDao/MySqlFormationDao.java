a9a0d632a6cb7588089e15bfc07c32b3989bde74
aminGandouz <amin1910@hotmail.com>
2021-02-04 12:09:34 +0100

    procedure done2 haha

--- src/model/dao/mySqlDao/MySqlFormationDao.java
+++ src/model/dao/mySqlDao/MySqlFormationDao.java
@@ -24,7 +24,7 @@ public class MySqlFormationDao implements FormationDao {
     public static MySqlFormationDao getInstance() {
         return instance;
     }
-    
+
     private MySqlFormationDao() {
     }
 
@@ -34,7 +34,7 @@ public class MySqlFormationDao implements FormationDao {
         Connection c = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String sql = " SELECT `IdFormation`, `Intitule`, `Prix`, `Duree`, `MaxParticipant`, `NbreParticipantMin`, `enable` FROM `formation` WHERE Intitule LIKE ? ";
+        String sql = " SELECT `IdFormation`, `Intitule`, `Prix`, `Duree`, `MaxParticipant`, `NbreParticipantMin`, `enable` FROM `formation` WHERE Intitule LIKE ? and enable = ? ";
 
         try {
             c = MySqlDaoFactory.getInstance().getConnection();
@@ -44,7 +44,7 @@ public class MySqlFormationDao implements FormationDao {
 
             while (rs.next()) {
                 Formation formation = new Formation(rs.getInt(1),
-                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5),rs.getInt(6));
+                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
                 listFormation.add(formation);
             }
         } catch (SQLException e) {
@@ -59,7 +59,7 @@ public class MySqlFormationDao implements FormationDao {
     }
 
     @Override
-    public void ajoutFormation(Formation f ) {
+    public void ajoutFormation(Formation f) {
         Connection c = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -87,43 +87,75 @@ public class MySqlFormationDao implements FormationDao {
     }
 
     @Override
-    public void effacerFormation(String formation) {
+    public Boolean effacerFormation(Formation formation) {
+        Boolean ok = false;
         Connection c = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String sql = " update `formation` set Enable = ? WHERE `intitule` = ? ";
+        String ifFormationAlone = " select IdFormation from formation where formation.IdFormation= ? and formation.IdFormation not in (select IdFormation from session) ";
+        String ifSessionExist = " select IdSession from session where session.IdFormation = ? and DATEDIFF(session.DateDebut,now())>? and session.IdSession not in(select idSession from inscrire) ";
+        String sql = " update `formation` set Enable = ? WHERE `IdFormation` = ? ";
+        //String sql2 = " update `session` set Enable = ? WHERE session.IdSession = ? ";
         try {
             c = MySqlDaoFactory.getInstance().getConnection();
-            ps = c.prepareStatement(sql);
-            ps.setBoolean(1, false);
-            ps.setString(2, formation);
-            ps.executeUpdate();
+            ps = c.prepareStatement(ifFormationAlone);
+            ps.setInt(1, formation.getIdFormation());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                ps = c.prepareStatement(sql);
+                ps.setBoolean(1, false);
+                ps.setInt(2, formation.getIdFormation());
+                ps.executeUpdate();
+                ok = true;
+            } else {
+                ps = c.prepareStatement(ifSessionExist);
+                ps.setInt(1, formation.getIdFormation());
+                ps.setInt(2, 0);
+                rs = ps.executeQuery();
+
+                while (rs.next()) {
+//                    int idSess = rs.getInt(1);
+//                    ps = c.prepareStatement(sql2);
+//                    ps.setBoolean(1, false);
+//                    ps.setInt(2, idSess);
+//                    ps.executeUpdate();
+
+                    ps = c.prepareStatement(sql);
+                    ps.setBoolean(1, false);
+                    ps.setInt(2, formation.getIdFormation());
+                    ps.executeUpdate();
+                    ok = true;
+                }
+            }
+
         } catch (SQLException e) {
             //System.err.println(" Probleme avec la requete SQL effacerFormation(String formation); \n" + e.getMessage());
-            //System.out.println("Probleme avec la requete SQL effacerFormation(String formation)");
+            System.out.println("Probleme avec la requete SQL effacerFormation(String formation)");
             e.printStackTrace();
         } finally {
             MySqlDaoFactory.closeResultSet(rs);
             MySqlDaoFactory.closeStatement(ps);
             MySqlDaoFactory.closeConnection(c);
         }
+        return ok;
     }
-    /* TODO verifier dans le code car j'en recupère plusieurs */
+
     @Override
     public Formation getFormationAvecNom(String nomFormation) {
         Formation formation = null;
         Connection c = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String sql = " SELECT `IdFormation`, `Intitule`, `Prix`, `Duree`, `MaxParticipant`, `NbreParticipantMin`, `enable` FROM `formation` WHERE Intitule LIKE ? ";
+        String sql = " SELECT `IdFormation`, `Intitule`, `Prix`, `Duree`, `MaxParticipant`, `NbreParticipantMin`, `enable` FROM `formation` WHERE Intitule LIKE ? and enable = ?  ";
         try {
             c = MySqlDaoFactory.getInstance().getConnection();
             ps = c.prepareStatement(sql);
             ps.setString(1, "%" + nomFormation + "%");
+            ps.setBoolean(2, true);
             rs = ps.executeQuery();
             if (rs.next()) {
                 formation = new Formation(rs.getInt(1),
-                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5),rs.getInt(6));
+                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
             }
         } catch (SQLException e) {
             System.out.println("Probleme avec la requete SQL getFormationAvecNom(String nomFormation)");
@@ -135,6 +167,7 @@ public class MySqlFormationDao implements FormationDao {
         }
         return formation;
     }
+
     @Override
     public Formation getFormationById(Integer idFormation) {
         Formation formation = null;
@@ -149,7 +182,7 @@ public class MySqlFormationDao implements FormationDao {
             rs = ps.executeQuery();
             if (rs.next()) {
                 formation = new Formation(rs.getInt(1),
-                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5),rs.getInt(6));
+                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
             }
 
         } catch (SQLException e) {
@@ -162,7 +195,6 @@ public class MySqlFormationDao implements FormationDao {
         }
         return formation;
     }
-
 
     @Override
     public void updateFormation(Formation formation) {
@@ -188,16 +220,16 @@ public class MySqlFormationDao implements FormationDao {
             MySqlDaoFactory.closeConnection(c);
         }
     }
-    /* TODO si le formateur prend un status le rajouter */
+
     @Override
     public List<Session> getListSessionAvecNom(Formateur formateur) {
         List<Session> listSession = new ArrayList<>();
         Connection c = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        String sql = " select s.IdSession,s.IdFormation,f.Intitule,f.Prix,f.Duree,f.MaxParticipant,f.NbreParticipantMin,  s.IdFormateur,u.Nom,u.Prenom,u.Adresse,u.Telephone,u.Email,u.Login,u.Password,ro.IdRole,ro.DenomRole,\n " +
-" st.IdStatus,st.DenomStatus,s.DateDebut,s.DateFin,lo.IdLocal,lo.DenomLocal \n " +
-" from session as s,formation as f,locaux as lo,utilisateur as u,roles as ro,status as st where s.IdFormation = f.IdFormation and s.IdFormateur = u.IdUtilisateur and s.Local = lo.IdLocal and u.Role = ro.IdRole and u.Status = st.IdStatus and s.IdFormateur =  ? ";
+        String sql = " select s.IdSession,s.IdFormation,f.Intitule,f.Prix,f.Duree,f.MaxParticipant,f.NbreParticipantMin,  s.IdFormateur,u.Nom,u.Prenom,u.Adresse,u.Telephone,u.Email,u.Login,u.Password,ro.IdRole,ro.DenomRole,\n "
+                + " st.IdStatus,st.DenomStatus,s.DateDebut,s.DateFin,lo.IdLocal,lo.DenomLocal \n "
+                + " from session as s,formation as f,locaux as lo,utilisateur as u,roles as ro,status as st where s.IdFormation = f.IdFormation and s.IdFormateur = u.IdUtilisateur and s.Local = lo.IdLocal and u.Role = ro.IdRole and u.Status = st.IdStatus and s.IdFormateur =  ? ";
 
         try {
             c = MySqlDaoFactory.getInstance().getConnection();
@@ -205,13 +237,13 @@ public class MySqlFormationDao implements FormationDao {
             ps.setInt(1, formateur.getIdUtilisateur());
             rs = ps.executeQuery();
             while (rs.next()) {
-                Session session = new Session(rs.getInt(1),                   
+                Session session = new Session(rs.getInt(1),
                         new Formateur(rs.getInt(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15),
                                 new Role(rs.getInt(16), rs.getString(17))),
                         rs.getDate(20), rs.getDate(21),
                         new Local(rs.getInt(22), rs.getString(23)));
                 listSession.add(session);
-            }//new Status(rs.getInt(18), rs.getString(19))
+            }
         } catch (SQLException e) {
             System.out.println("Probleme avec la requete SQL getListSessionAvecNom(Formateur formateur)");
             e.printStackTrace();
@@ -221,5 +253,33 @@ public class MySqlFormationDao implements FormationDao {
             MySqlDaoFactory.closeConnection(c);
         }
         return listSession;
+    }
+
+    @Override
+    public Formation getFormationByName(String nom) {
+        Formation formation = null;
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = " SELECT `IdFormation`, `Intitule`, `Prix`, `Duree`, `MaxParticipant`, `NbreParticipantMin`, `enable` FROM `formation` WHERE Intitule = ? ";
+        try {
+            c = MySqlDaoFactory.getInstance().getConnection();
+            ps = c.prepareStatement(sql);
+            ps.setString(1, nom);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                formation = new Formation(rs.getInt(1),
+                        rs.getString(2), rs.getFloat(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Probleme avec la requete SQL getFormationById(Integer idFormation)");
+            e.printStackTrace();
+        } finally {
+            MySqlDaoFactory.closeResultSet(rs);
+            MySqlDaoFactory.closeStatement(ps);
+            MySqlDaoFactory.closeConnection(c);
+        }
+        return formation;
     }
 }
